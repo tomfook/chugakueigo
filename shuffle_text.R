0827ca271d98d5e9e66d47a220119f2d3244f370
tomfook <tomfook@gmail.com>
2025-07-17 15:18:08 +0900

    more random

--- shuffle_text.R
+++ shuffle_text.R
@@ -5,40 +5,42 @@ shuffleQuestion <- function(q, a){
     shuffle_text <- function(keywords, qa){
       q <- qa$q
       a <- qa$a
-      if (b == sample(0:1, 1)){
-        for (item in keywords){
-          if (grepl(item[1], a)){
-            q <- gsub(item[3], item[4], q)
-            a <- gsub(item[1], item[2], a)
-          }
-        }
-      } else {
-        q <- qa$q
-        a <- qa$a
+      
+      bs <- runif(length(keywords)) < 1/2
+      kw2 <- keywords[bs]
+      for (i in seq_along(kw2)){
+         item <- keywords[i]
+         if (grepl(item[1], a)){
+           q <- gsub(item[3], item[4], q)
+           a <- gsub(item[1], item[2], a)
+         } else if (grepl(item[2], a)){
+           q <- gsub(item[4], item[3], q)
+           a <- gsub(item[2], item[1], a)
+         }
       }
       return (list(q = q, a=a))
     }
     
     keywords1 <- list(
-      c(" he ", " she ", "彼", "彼女"),
-      c("He ", "She ", "彼", "彼女"),
-      c(" his ", " her ", "彼", "彼女"),
-      c(" him\\b", " her ", "彼", "彼女"),
-      
       c(" every day ", " every week ", "毎日", "毎週"),
-      c("lunch", "dinner", "昼食", "夕食"),
-      c("two hours", "three hours", "２時間", "３時間"),
+      c(" every morning ", " every evening ", "毎朝", "毎晩"),
+      c(" every month ", " every year ", "毎月", "毎年"),
+      c(" lunch ", " dinner ", "昼食", "夕食"),
+      c(" two hours ", " three hours ", "２時間", "３時間"),
+      c(" summer ", " winter ", "夏", "冬"),
       
-      c("\\bgolf\\b", "tennis", "ゴルフ", "テニス"),
-      c("\\bpiano\\b", "violin", "ピアノ", "バイオリン"),
+      c(" golf ", " tennis ", "ゴルフ", "テニス"),
+      c(" piano ", " violin ", "ピアノ", "バイオリン"),
       c(" guitar ", " piano ", "ギター", "ピアノ"),
       c(" green tea ", " black tea ", "緑茶", "紅茶"),
       c(" coffee ", " tea ", "コーヒー", "お茶"),
       c(" taxi ", " bus ", "タクシー", "バス"),
+      c(" orange juice ", " apple juice ", "オレンジジュース", "アップルジュース"),
       
-      c("\\bBill\\b", "Steve", "ビル", "スティーブ"),
+      c(" Bill ", " Steve ", "ビル", "スティーブ"),
       c("Waseda", "Keio", "早稲田", "慶応"),
       c("Fuji Bank", "Mizuho Bank", "富士銀行", "みずほ銀行"),
+      c(" economics ", " physics ", "経済学", "物理学"),
       
       c("Greek", "Turkish", "ギリシャ", "トルコ"),
       c("Sweden", "Finland", "スウェーデン", "フィンランド"),
@@ -47,24 +49,10 @@ shuffleQuestion <- function(q, a){
       c("Yokohama", "Kobe", "横浜", "神戸"),
       c("London", "Paris", "ロンドン", "パリ"),
       c("Russian", "Ukrainian", "ロシア", "ウクライナ"),
-      c("Germany", "Italy", "ドイツ", "イタリア")
+      c("Germany", "Italy", "ドイツ", "イタリア"),
+      c("Portuguese", "Spanish", "ポルトガル", "スペイン")
     )
     qa_mod <- shuffle_text(keywords1, qa_mod)
-    
-    # keyword1と同時にできない
-    keywords2 <- list(
-      c(" she ", " he ", "彼女", "彼"),
-      c("She ", "He ", "彼女", "彼")
-    )
-    qa_mod <- shuffle_text(keywords2, qa_mod)
-    
-    # 必ずペアで実行する
-    keywords3 <- list(
-      c("\\bfather\\b", "TEMP_FATHER", "父", "TEMP_父"),
-      c("\\bmother\\b", "father", "母", "父"),
-      c("TEMP_FATHER", "mother", "TEMP_父", "母")
-    )
-    qa_mod <- shuffle_text(keywords3, qa_mod)
     
     return(list(q = qa_mod$q, a = qa_mod$a))
 }
